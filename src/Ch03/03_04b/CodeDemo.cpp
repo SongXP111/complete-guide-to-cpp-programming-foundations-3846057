8c5ea70d88d43ec379f44f60562ab5ba2270fbd7
SongXP111 <163683058+SongXP111@users.noreply.github.com>
2025-04-16 23:06:44 +0000

    chapter 3 finished

--- src/Ch03/03_04b/CodeDemo.cpp
+++ src/Ch03/03_04b/CodeDemo.cpp
@@ -12,16 +12,20 @@ int main(){
     std::cout << "Initial Lives: " << player_lives << std::endl;
 
     // Postfix increment
-    std::cout << "Score using postfix increment: " << player_score << std::endl;
+    // returns the value before incrementing
+    std::cout << "Score using postfix increment: " << player_score++ << std::endl;
 
     // Prefix increment
-    std::cout << "Score using prefix increment: " << player_score << std::endl;
+    // returns the value after incrementing
+    std::cout << "Score using prefix increment: " << ++player_score << std::endl;
 
     // Postfix decrement
-    std::cout << "Lives using postfix decrement: " << player_lives << std::endl;
+    // returns the value before decrementing
+    std::cout << "Lives using postfix decrement: " << player_lives-- << std::endl;
 
     // Prefix decrement
-    std::cout << "Lives using prefix decrement: " << player_lives << std::endl;
+    // returns the value after decrementing
+    std::cout << "Lives using prefix decrement: " << --player_lives << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
